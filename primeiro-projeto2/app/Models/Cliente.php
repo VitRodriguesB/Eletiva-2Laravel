d6798b41b35cc06c1b51717e49bdf799e61bb757
VitRodriguesB <vitornety2098@gmail.com>
2025-09-02 20:36:45 -0300

    up
    up

---
+++ primeiro-projeto2/app/Models/Cliente.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    protected $teble = "clientes";
+    public $increment = true;
+}
--- primeiro-projeto2/routes/web.php
+++ primeiro-projeto2/routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrimeiraController;
+use App\http\Controllers\ClienteController;
+
+Route::resource('clientes', ClienteController::class);
+//Listar Clientes - GET / clientes -- Route::get('\clientes',[CienteController::class, 'index'])
+// Abrir formulario para inserir registro - GET /cliente/creates/creat -- 
+// Salvar dados - POST /clientes -- metodo store
+// Mostrar dados do registro - GET /clientes/{id_cliente} -- método show
+// Abrir formulario para editar registro - GET /cliente/{id_cliente}/edit -- método
+// Salvar alterações - PUT /clientes/{id_cliente}
+
+
+
+
+
+
 
 Route::get('/teste', [PrimeiraController::class, "teste"]);
 
@@ -11,5 +26,5 @@ Route::post('/exemplo_resposta', [PrimeiraController::class, "resposta"]);
 Route::get("/exercicio2", [PrimeiraController::class, "exercicio2"]);
 Route::post("/resposta_exercicio2", [PrimeiraController::class, "respExercicio2"]);
 
-Router::get("\clientes",function() {return view ("cliente.index");});
+Route::get("\clientes",function() {return view ("cliente.index");});
 
